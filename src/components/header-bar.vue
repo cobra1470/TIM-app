d4469d5aab1bd97194e20810be58e2cc515a466e
cobra1470 <lhg2work@sina.com>
2018-04-24 12:03:48 +0800

    重新布置环境

---
+++ src/components/header-bar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'headerbar',
+    props: ['msg']
+}
+</script>
+
+<style lang="scss" scoped>
+    #header{
+        width: 100%;
+        height: 60px;
+    }
+</style>
---
+++ src/components/message.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="header">
+        <headerbar :msg="myMsg"></headerbar>
+    </div>
+</template>
+
+<script>
+// import headerbar from "./header-bar.vue"
+
+
+export default {
+    name: 'message',
+    // components: { headerbar },
+    data () {
+        return {
+            myMsg: '12333'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    #header{
+        width: 100%;
+        height: 60px;
+    }
+</style>
